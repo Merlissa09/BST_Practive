ed20fabd9c1a75eeeb51f279165bf120b8d4622a
MelissaH <iheartumedicdroid@gmail.com>
2024-04-23 08:13:29 -0700

    Update

---
+++ BinarySearchTree.java
@@ -0,0 +1,105 @@
+public class BinarySearchTree {
+    Node root;
+
+    public void insert(Node node) {
+        root = insertHelper(root, node);
+    }
+    
+    private Node insertHelper(Node root, Node node) {
+        int data = node.data;
+
+        if (root == null) {
+            root = node;
+            return root;
+        } else if (data < root.data) {
+            root.left = insertHelper(root.left, node);
+        } else {
+            root.right = insertHelper(root.right, node);
+        }
+
+        
+        return root;
+    }
+
+    public void display() {
+        displayHelper(root);
+    }
+    
+    private void displayHelper(Node root) {
+        if(root != null) {
+            displayHelper(root.left);
+            System.out.println(root.data);
+            displayHelper(root.right);
+        }
+    }
+    
+    public boolean search(int data) {
+        return searchHelper(root, data);
+    }
+    
+    private boolean searchHelper(Node root, int data) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == data) {
+            return true;
+        } else if (data < root.data) {
+            searchHelper(root.left, data);
+        } else {
+            searchHelper(root.right, data);
+        }
+
+        return false;
+    }
+
+    public void remove(int data) {
+        if (search(data)) {
+            removeHelper(root, data);
+        } else {
+            System.out.println(data + " could not be found");
+        }
+    }
+    
+    public Node removeHelper(Node root, int data) {
+        if (root == null) {
+            return root;
+        } else if(data < root.data) {
+            root.left = removeHelper(root.left, data);
+        } else if (data > root.data) {
+            root.right = removeHelper(root.right, data);
+        } else { // found our node
+            if (root.left == null && root.right == null) {
+                root = null;
+            } else if (root.right != null) {
+                // find a successor to replace this node
+                root.data = successor(root);
+                root.right = removeHelper(root.right, root.data);
+            } else { // find a predecessor to replace this node
+                root.data = predecessor(root);
+                root.left = removeHelper(root.left, root.data);
+            }
+        }
+        
+        return root;
+    }
+
+    private int successor(Node root) { // find the least value below the right child of this root node
+        root = root.right;
+
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root.data;
+    }
+
+    private int predecessor(Node root) { // find the greatest
+        root = root.left;
+
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root.data;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(4));
+        tree.insert(new Node(0));
+        tree.display();
+        System.out.println("5 " + tree.search(5));
+        System.out.println("80 " + tree.search(80));
+
+    }
+}
